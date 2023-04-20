44a0ee5eb89b05ffb629c9b60319cbe376133bab
mzieve <morriszieve@outlook.com>
2023-04-20 02:33:51 -0700

    Removed format 2 files, replaced format 1 files with proper ones, debuged and fixed cat_id (321-480), and updated bar charts

--- data_allocation.py
+++ data_allocation.py
@@ -5,22 +5,7 @@ import pandas as pd
 # Format 1 Data Frames
 user_log = pd.read_excel('Dataset/user_log_format1.xlsx')
 
-# Format 2 Data Frames
-format2_train_data = pd.read_excel('Dataset/train_format2.xlsx')
-format2_test_data = pd.read_excel('Dataset/test_format2.xlsx')
-
 # Group 3 Range
-item_ids = list(range(321, 481))
+user_log = user_log[(user_log['cat_id'] >= 321) & (user_log['cat_id'] <= 480)]
 
-# Remove item_id from format 1
-user_log = user_log[~user_log['item_id'].isin(item_ids)]
 user_log.to_excel('Dataset/updated_user_log_format1.xlsx', index=False)
-
-# Remove item_id from format 2 activity logs
-format2_train_data['activity_log'] = format2_train_data['activity_log'].fillna('').apply(lambda x: '#' + '#'.join([s for s in x.split('#') if len(s.split(':')) > 1 and int(s.split(':')[0]) not in item_ids]) + '#')
-format2_train_data = format2_train_data[format2_train_data['activity_log'] != '##']
-format2_train_data.to_excel('Dataset/update_train_format2.xlsx', index=False)
-
-format2_test_data['activity_log'] = format2_test_data['activity_log'].fillna('').apply(lambda x: '#' + '#'.join([s for s in x.split('#') if len(s.split(':')) > 1 and int(s.split(':')[0]) not in item_ids]) + '#')
-format2_test_data = format2_test_data[format2_test_data['activity_log'] != '##']
-format2_test_data.to_excel('Dataset/update_test_format2.xlsx', index=False)
--- visualization.py
+++ visualization.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-user_logs = pd.read_excel('Dataset/user_log_format1.xlsx')
+user_logs = pd.read_excel('Dataset/updated_user_log_format1.xlsx')
 user_profile = pd.read_excel('Dataset/user_info_format1.xlsx')
 train_df = pd.read_excel('Dataset/train_format1.xlsx')
 test_df = pd.read_excel('Dataset/test_format1.xlsx')
